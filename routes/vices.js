9cddb1e6481675bf363aca2dbfb68bfa1e81ccf5
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-06 13:36:41 +1100

    Introduce pug

--- routes/vices.js
+++ routes/vices.js
@@ -10,12 +10,18 @@ router.get('/', function (req, res, next) {
 });
 */
 
-router.get('/pug', function (req, res, next) {
+router.get('/notpug', function (req, res, next) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
   res.write('<h1>Pug, not Pug !</h1>');
   res.end();
+});
+
+router.get('/pug', function (req, res, next) {
+  res.render('vices', {
+    vices: ['Wine', 'Women', 'Song']
+  });
 });
 
 router.get('/', (req, res, next) => {

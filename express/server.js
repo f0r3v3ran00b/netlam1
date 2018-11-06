0108092ac4e4da9a34cd47a329d19dd24dd23ccf
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-06 13:32:25 +1100

    Introduce pug

--- express/server.js
+++ express/server.js
@@ -22,7 +22,8 @@ router.post('/', (req, res) => res.json({
 }));
  */
 app.use(bodyParser.json());
-//app.use('/.netlify/functions/server', router); // path must route to lambda
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use('/.netlify/functions/server', vicesRouter); // path must route to lambda
 
 module.exports = app;

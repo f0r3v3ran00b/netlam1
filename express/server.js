fddc54bf67d0a8d9d3c346889fdb06845f036dc6
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-06 13:24:57 +1100

    Introduce pug

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 var vicesRouter = require('../routes/vices');
 
 const router = express.Router();
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
@@ -20,7 +20,7 @@ router.get('/another', (req, res) => res.json({
 router.post('/', (req, res) => res.json({
   postBody: req.body
 }));
-
+ */
 app.use(bodyParser.json());
 //app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/.netlify/functions/server', vicesRouter); // path must route to lambda
--- routes/vices.js
+++ routes/vices.js
@@ -10,11 +10,17 @@ router.get('/', function (req, res, next) {
 });
 */
 
+router.get('/pug', function (req, res, next) {
+  res.render('vices', {
+    vices: ['Wine', 'Women', 'Song']
+  });
+});
+
 router.get('/', (req, res, next) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  res.write('<h1>Hello from Express.js using routes !!!</h1>');
+  res.write('<h1>Hello from Express.js using routes !</h1>');
   res.end();
 });
 

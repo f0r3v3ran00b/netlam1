2893cf1225cd1f68dec012129f93bbd223143c16
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-06 12:56:57 +1100

    Testing...

--- express/server.js
+++ express/server.js
@@ -4,17 +4,26 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+var vicesRouter = require('./routes/vices');
+
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
   res.write('<h1>Hello from Express.js!!!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({route: req.originalUrl}));
-router.post('/', (req, res) => res.json({postBody: req.body}));
+router.get('/another', (req, res) => res.json({
+  route: req.originalUrl
+}));
+router.post('/', (req, res) => res.json({
+  postBody: req.body
+}));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
+//app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/.netlify/functions/server', vicesRouter); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
---
+++ routes/vices.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+/*
+router.get('/', function (req, res, next) {
+    res.render('vices', {
+        vices: ['Wine', 'Women', 'Song']
+    });
+});
+*/
+
+router.get('/', (req, res, next) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<h1>Hello from Express.js using routes !!!</h1>');
+  res.end();
+});
+
+
+module.exports = router;
